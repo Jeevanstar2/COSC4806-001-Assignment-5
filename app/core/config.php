4f149d14ef8319ac925fa6c4e179d8487e8021ff
Jeevanstar2 <jeevanthomasjaison01@algomau.ca>
2025-07-17 21:38:29 +0000

    made it a bit cooler

--- app/core/config.php
+++ app/core/config.php
@@ -6,7 +6,6 @@ define('CORE', APPS . DS . 'core');
 define('CONTROLLERS', APPS . DS . 'controllers');
 define('MODELS', APPS . DS . 'models');
 define('VIEWS', APPS . DS . 'views');
-
 define('DB_HOST',     'c0tme.h.filess.io');
 define('DB_USER',     'COSC4806001JS2_figurewhom');
 define('DB_PASS',     $_ENV['DB_PASS']);
