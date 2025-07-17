0cf5eff684fa078f6e17b3c4da8483827900ab41
Jeevanstar2 <jeevanthomasjaison01@algomau.ca>
2025-07-17 21:30:52 +0000

    making it look a bit cleaner

--- app/controllers/Logout.php
+++ app/controllers/Logout.php
@@ -1,6 +1,8 @@
 <?php
-class Logout extends Controller {
-    public function index(): void {
+class Logout extends Controller 
+{
+    public function index(): void 
+    {
         $_SESSION = [];
         session_destroy();
         $this->redirect('/login');
